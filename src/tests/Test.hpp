a0a1c3c54456bdbfadf563638651b6df864bfc65
R4WLYX <r4wlyx234@gmail.com>
2023-06-24 17:38:13 +0700

    Idk what this is anymore

---
+++ src/tests/Test.hpp
@@ -0,0 +1,44 @@
+#ifndef TEST_HPP
+#define TEST_HPP
+
+#include <vector>
+#include <string>
+#include <functional>
+
+#include "imgui/imgui.h"
+
+namespace test {
+class Test {
+public:
+    Test() {}
+    virtual ~Test() {}
+
+    virtual void on_update(float deltaTime) {}
+    virtual void on_render() {}
+    virtual void on_imgui_render() {}
+};
+
+class TestMenu : public Test {
+private:
+    Test*& m_currentTest;
+    std::vector<std::pair<std::string, std::function<Test*()>>> m_tests;
+public:
+    TestMenu(Test*& currentTestPointer)
+        : m_currentTest(currentTestPointer)
+    {}
+
+    void on_imgui_render() override {
+        for (auto& test : m_tests) {
+            if (ImGui::Button(test.first.c_str()))
+                m_currentTest = test.second();
+        }
+    }
+
+    template<typename T>
+    void register_test(const std::string& name) {
+        m_tests.push_back(std::make_pair(name, [](){ return new T(); }));
+    }
+};
+}
+
+#endif
---
+++ src/tests/TestClearColor.hpp
@@ -0,0 +1,30 @@
+#ifndef TEST_CLEAR_COLOR_HPP
+#define TEST_CLEAR_COLOR_HPP
+
+#include "Test.hpp"
+
+#include "Keywords.hpp"
+#include "imgui/imgui.h"
+
+namespace test {
+class TestClearColor : public Test {
+private:
+    float m_clearColor[4];
+public:
+    TestClearColor() 
+        : m_clearColor{1.0f, 1.0f, 1.0f, 1.0f}
+    {}
+    ~TestClearColor() {}
+
+    virtual void on_update(float deltaTime) override {}
+    virtual void on_render() override {
+        GLCall(glClearColor(m_clearColor[0], m_clearColor[1], m_clearColor[2], m_clearColor[3]));
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));
+    }
+    virtual void on_imgui_render() override {
+        ImGui::ColorEdit4("Clear Color", m_clearColor);
+    }
+};
+}
+
+#endif
